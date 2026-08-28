6491529a67f30f1a58942e49eefd6fd7ce367661
Phillip Chin <ekkus93@gmail.com>
2026-08-28 00:32:02 -0700

    P1-002: export language config schema

--- src/tiny_qwen_coder/languages/__init__.py
+++ src/tiny_qwen_coder/languages/__init__.py
@@ -1,1 +1,17 @@
 """Programming-language specifications and registry services."""
+
+from tiny_qwen_coder.languages.schema import (
+    ConfigReferences,
+    LanguageConfig,
+    LanguageHookReferences,
+    RepositoryDetectionSignals,
+    SystemPromptSpec,
+)
+
+__all__ = [
+    "ConfigReferences",
+    "LanguageConfig",
+    "LanguageHookReferences",
+    "RepositoryDetectionSignals",
+    "SystemPromptSpec",
+]
